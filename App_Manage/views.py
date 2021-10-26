07e1d91d55fafcae00c349b1f749f9394daccb37
xumeijie <xcdh560@foxmail.com>
2021-10-27 01:24:37 +0800

    ajax问题待解决

--- App_Manage/views.py
+++ App_Manage/views.py
@@ -1,3 +1,5 @@
+import os
+import json
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.views import View
 from django.contrib.auth.models import User
@@ -5,8 +7,7 @@ from django.contrib import auth
 from django.core.paginator import Paginator
 from django.views.decorators.clickjacking import xframe_options_sameorigin
 from django.conf import settings as sys
-import os
-import json
+from bs4 import BeautifulSoup
 from App_Blog.models import Article, Tags, Category, LeaveMsg
 from App_Essay.models import Essay
 
@@ -18,37 +19,82 @@ def has_permission(request):
 
 
 class ArticleManage(View):
+    def __init__(self):
+        self.tagsObj = Tags.objects.all()
+        self.cateObj = Category.objects.all()
+        self.edit_dict = None
+
+    def get_page_obj(self, request):
+        # 分页
+        page = request.GET.get('page')
+        article_obj = Article.objects.all()
+        paginator = Paginator(article_obj, 20)
+        pageObj = paginator.get_page(page)
+        return pageObj
+
     # 文章管理
     def get(self, request):
         if has_permission(request):
             # 分类和标签
-            tagsObj = Tags.objects.all()
-            cateObj = Category.objects.all()
-            # 分页
-            page = request.GET.get('page')
-            article_obj = Article.objects.all()
-            paginator = Paginator(article_obj, 8)
-            pageObj = paginator.get_page(page)
+            tagsObj = self.tagsObj
+            cateObj = self.cateObj
+            edit_dict = self.edit_dict
+            pageObj = self.get_page_obj(request)
             return render(request, 'managehtml/article.html', locals())
         return redirect(reverse('blog:home'))
 
     def post(self, request):
         if has_permission(request):
-            print('POAT请求到来！')
-            post_type = request.GET.get('type')
-            if post_type == 'add':
+            post_type = 'add'
+            if post_type:
                 title = request.POST.get('article_title')
-                tag = request.POST.get('tags')
+                tag_id = request.POST.get('tags')
                 cate = request.POST.get('category')
                 body = request.POST.get('article_body')
-                desc = body[3:-3]
-                if all([title, tag, cate, body]):
-                    Article.objects.create()
-            elif post_type == 'edit':
-                print('收到编辑请求')
-            elif post_type == 'delete':
-                print('收到删除请求')
-            return None
+                status = False
+                tagsObj = self.tagsObj
+                cateObj = self.cateObj
+                pageObj = self.get_page_obj(request)
+                if all([title, tag_id, cate, body]):
+                    soup = BeautifulSoup(body, 'html.parser')
+                    # 非法标签删除操作 防止XSS攻击
+                    for html_tag in soup.find_all():
+                        if html_tag.name == 'script':
+                            html_tag.decompose()
+                    desc = soup.text[0:150]  # 文章简介描述信息
+                    new_body = str(soup)  # 文章内容
+                    new_article_obj = Article.objects.create(author_id=request.user.pk,
+                                                             title=title,
+                                                             desc=desc,
+                                                             body=new_body,
+                                                             category_id=cate,
+                                                             )
+                    new_article_obj.tags.add(tag_id)
+                    # tag = Tags.objects.filter(id=tag_id).first()
+                    # new_article_obj.tags.add(tag)
+                    status = True
+                    status_msg = 'success'
+                    return render(request, 'managehtml/article.html', locals())
+                else:
+                    status_msg = 'error'
+                    return render(request, 'managehtml/article.html', locals())
+            if request.is_ajax():
+                ajax_type = request.POST.get('ajax_type')
+                article_id = request.POST.get('pk')
+                if ajax_type == 'delete':
+                    Article.objects.filter(id=article_id).first().delete()
+                    return HttpResponse(json.dumps('success'))
+                if ajax_type == 'change':
+                    article_obj = Article.objects.filter(id=article_id).first()
+                    self.edit_dict = {
+                        'title': article_obj.title,
+                        'body': article_obj.body,
+                        'category': article_obj.category
+                    }
+                    return HttpResponse(json.dumps('success'))
+            else:
+                cnm = '大佬不要攻击我!'
+                return HttpResponse(json.dumps(cnm))
         return redirect(reverse('blog:home'))
 
 
--- static/managestatic/js/article.js
+++ static/managestatic/js/article.js
@@ -1,23 +1,68 @@
 $('#myButton').on('click', function () {
-	let $btn = $(this).button('loading')
-	$btn.button('提交')
+    let $btn = $(this).button('loading')
+    $btn.button('提交')
 })
 
 tinyMCE.init({
-	selector: '#tinymec',  // textarea ID
-	language_url: '/static/tinymce/js/tinymce/langs/zh_CN.js',
-	language: 'zh_CN',  // 语言
-	directionality: 'ltr',  // 光标从左到右，rtl表示从右到左
-	browser_spellcheck: true,
-	contextmenu: false,
-	height: '700',
-	width: '770',
-	plugins: [
-		'advlist autolink lists link image charmap print preview anchor',
-		'searchreplace visualblocks code fullscreen',
-		'insertdatetime media table contextmenu paste imagetools wordcount',
-		'code blockformats'
-	],
-	toolbar: 'insertfile undo redo | styleselect | bld italic | alignleft aligncenter',
+    selector: '#tinymec',  // textarea ID
+    language_url: '/static/tinymce/js/tinymce/langs/zh_CN.js',
+    language: 'zh_CN',  // 语言
+    directionality: 'ltr',  // 光标从左到右，rtl表示从右到左
+    browser_spellcheck: true,
+    contextmenu: false,
+    height: '700',
+    width: '770',
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste imagetools wordcount',
+        'code blockformats'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bld italic | alignleft aligncenter',
 });
-tinyMCE.activeEditor.getBody().style.backgroundColor = '#000';
+
+// 删除功能
+function delete_article(pk) {
+    let sure = confirm('确定要删除这篇文章吗？');
+    if (!sure) {
+        return;
+    }
+    $.ajax({
+        url: '',
+        type: 'post',
+        data: {
+            'csrfmiddlewaretoken': $("[name='csrfmiddlewaretoken']").val(),
+            'ajax_type': 'delete',
+            'pk': pk
+        },
+        success: function (data) {
+            var del_status = JSON.parse(data);
+            if (del_status === 'success') {
+                window.location.reload();
+            }
+        }
+    })
+}
+
+// 编辑功能
+function change_article(pk) {
+    let sure = confirm('确定要重新编辑这篇文章吗？');
+    if (!sure) {
+        return;
+    }
+    $.ajax({
+        url: '',
+        type: 'post',
+        data: {
+            'csrfmiddlewaretoken': $("[name='csrfmiddlewaretoken']").val(),
+            'ajax_type': 'change',
+            'pk': pk
+        },
+        success: function (data) {
+            var edit_status = JSON.parse(data);
+            if (edit_status === 'success') {
+                window.location.reload();
+            }
+        }
+    })
+}
